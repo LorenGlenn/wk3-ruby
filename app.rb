36ef41bc2c2ffb73967b3f19b76c1ad4bcf0c1af
Loren Glenn <lorencglenn@gmail.com>
2017-01-24 17:16:23 -0800

    refactor app.rb

--- app.rb
+++ app.rb
@@ -11,3 +11,68 @@ DB = PG.connect({:dbname => "salon"})
 get("/") do
   erb(:index)
 end
+
+post("/stylists") do
+  name = params.fetch("name")
+  hours = params.fetch("hours")
+  stylist = Stylist.new({:name => name, :hours => hours, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get("/stylist/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  erb(:stylist)
+end
+
+post("/clients") do
+  name = params.fetch("name")
+  phone = params.fetch("phone")
+  stylist_id = params.fetch("stylist_id").to_i()
+  client = Client.new({:name => name, :stylist_id => stylist_id, :phone => phone})
+  client.save()
+  @stylist = Stylist.find(stylist_id)
+  erb(:stylist)
+end
+
+get("/stylists_edit/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  erb(:stylist_edit)
+end
+
+patch("/stylists/:id") do
+  name = params.fetch("name")
+  hours = params.fetch("hours")
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.update({:name => name, :hours => hours})
+  erb(:stylist)
+end
+
+delete("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.delete()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get("/client_edit/:id") do
+  @client = Client.find(params.fetch("id").to_i())
+  erb(:client_edit)
+end
+
+patch("/clients/:id") do
+  name = params.fetch("name")
+  phone = params.fetch("phone")
+  client = Client.find(params.fetch("id").to_i())
+  client.update({:name => name, :phone => phone})
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+delete("/clients/:id") do
+  @client = Client.find(params.fetch("id").to_i())
+  @client.delete()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
